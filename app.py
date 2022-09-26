8a4bdeb9f79961ffa8d5914636e16eaf54b2cf7d
pfouille <px.fouille@gmail.com>
2022-09-26 15:29:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def entry_point():
-    return 'Hello World!'
-
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
