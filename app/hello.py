fe6a2cddcec2af6aeacc43d8a135c229a09c9ffc
pfouille <px.fouille@gmail.com>
2022-09-26 15:56:25 +0200

    Create hello.py

---
+++ app/hello.py
@@ -0,0 +1,28 @@
+# -*- coding:utf-8 -*-
+
+import platform
+import subprocess
+from flask import Flask, Response, request
+app = Flask(__name__)
+
+@app.route("/")
+def headers():
+    return '<br/>'.join(['%s => %s' % (key, value) for (key, value) in request.headers.items()])
+
+@app.route("/favicon.ico")
+def favicon():
+    resp = Response(status=200, mimetype='image/png')
+    return resp
+
+@app.route("/pyver")
+def pyver():
+    return platform.python_version()
+
+@app.route("/tag")
+def tag():
+    p = subprocess.Popen(['git', 'describe', '--tags', '--abbrev=0'], stdout=subprocess.PIPE)
+    p.wait()
+    return p.stdout.read()
+
+if __name__ == "__main__":
+    app.run()
